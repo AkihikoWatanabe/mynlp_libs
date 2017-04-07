e052fafc45e69ea9f519042fd439ce548ee7f7eb
Akihiko Watanabe <watanabe@lr.pi.titech.ac.jp>
2017-04-07 19:53:59 +0900

    [pipe.py] use gzip

--- lib/pipe.py
+++ lib/pipe.py
@@ -1,30 +1,31 @@
 # coding=utf-8
 
 import cPickle
+import gzip
 
 class Pipe:
    
     @staticmethod
-    def dump_data(frms, file_path):
+    def dump_data(frms, file_path, suffix=".pkl.gz"):
         """ dump data to file_path using cPickle
         Params:
             frms(list): dump data
             file_path(str): path to dump
         """
-        with open(file_path, 'w') as f:
-            [cPickle.dump(frm, f) for frm in frms]
-		
+        with gzip.open(file_path, 'wb') as gf:
+            [cPickle.dump(frm, gf, cPickle.HIGHEST_PROTOCOL) for frm in frms]
+        
     @staticmethod
     def load_data(file_path):
         """ load dump data from file_path
         Params:
             file_path(str): path for load
-		"""
+        """
         data = []
-        with open(file_path) as f:
-		    while True:
-			    try:
-				    data.append(cPickle.load(f))
-			    except EOFError:
-				    break
+        with gzip.open(file_path, 'rb') as gf:
+            while True:
+                try:
+                    data.append(cPickle.load(gf))
+                except EOFError:
+                    break
         return data[0] if len(data)==1 else data
